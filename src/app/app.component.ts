cf6a64f75717e6788e86c6de891cb8af9334b603
Anandhu Ajayakumar <aajayakumar@menervasoftware.com>
2018-02-05 19:45:55 +0530

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HotRegisterer } from "angular-handsontable";
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,61 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  data : any[];
+  columns: object[] = [
+    {data: 'id', title: 'Id'},
+    {data: 'name', title: 'Name'},
+    {data: 'location', title: 'Location'},
+  ];
+  x;
+  y;
+  value;
+  colWidths=[200,200,200];
+  instance: string = "hotInstance";
+  constructor(private _hotRegisterer: HotRegisterer,private _http: HttpClient) {}
+  selectCell($event){
+  const x = parseInt(this.x, 10);
+    const y = parseInt(this.y, 10);
+    const hot = this._hotRegisterer.getInstance(this.instance);
+
+    if (isNaN(x) || isNaN(y)) {
+      hot.deselectCell();
+      return false;
+    }
+
+    if (hot.selectCell(y, x)) {
+      $event.target.focus();
+
+    } else {
+      hot.deselectCell();
+    }
+  
+    hot.unlisten();
+  }
+
+  changeValue($event) {
+    const x = parseInt(this.x, 10);
+    const y = parseInt(this.y, 10);
+
+    if (isNaN(x) || isNaN(y)) {
+      return false;
+    }
+
+    const hot = this._hotRegisterer.getInstance(this.instance);
+
+    hot.setDataAtCell(y, x, $event.target.value);
+  }
+  load($event){
+    var hot=this._hotRegisterer.getInstance(this.instance);
+    this._http.get(`//www.mocky.io/v2/5a785cad2f00004711668f83`)
+      .subscribe((res: Response) => {
+        console.log(res);
+        this.data=res['users'];
+      });
+ }
+ change($event){
+  var hot=this._hotRegisterer.getInstance(this.instance);
+  hot.setDataAtCell(1,2,'500');
+  // hot.selectCell(2,2);
+ }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { HotTableModule } from 'angular-handsontable';
 import { AppComponent } from './app.component';
-
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http/src/client';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HotTableModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
